96fdf7a863c7c6bf3e78115ac605da331062e1f1
sorenrocks <furrymetaphor@gmail.com>
2025-04-29 12:22:42 -0300

    tailwind, social buttons

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,25 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'
+import tailwindcss from '@tailwindcss/vite'
+
+import icon from 'astro-icon'
 
 export default defineConfig({
-  redirects: { 
-      '/bsky': 'https://bsky.app/profile/soren.rocks',
-      '/github': 'https://github.com/sorenrocks',
-      '/twitter': 'https://twitter.com/sorenrocks',
-      '/refs': 'https://drive.google.com/drive/folders/1m2yGpN1UeGEGOsTOthRqPdLm1SSQaL1I'
-    },
-});
+  redirects: {
+    // socials
+    '/bsky': 'https://bsky.app/profile/soren.rocks',
+    '/discord': 'https://discord.com/users/525050108966993920',
+    '/github': 'https://github.com/sorenrocks',
+    '/telegram': 'https://t.me/sorenrocks',
+    '/twitter': 'https://twitter.com/sorenrocks',
+
+    // other
+    '/refs': 'https://drive.google.com/drive/folders/1m2yGpN1UeGEGOsTOthRqPdLm1SSQaL1I',
+  },
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
+
+  integrations: [icon()],
+})
