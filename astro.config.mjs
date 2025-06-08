147dbd829cd14fe10eab19a99c9e243ef88f5168
sorenrocks <furrymetaphor@gmail.com>
2025-06-08 18:31:29 -0300

    game links

--- astro.config.mjs
+++ astro.config.mjs
@@ -13,6 +13,13 @@ export default defineConfig({
     '/telegram': 'https://t.me/sorenrocks',
     '/twitter': 'https://twitter.com/sorenrocks',
 
+    // games
+    '/steam': 'https://steamcommunity.com/id/sorenrocks',
+    '/vrc': 'https://vrchat.com/home/user/usr_179f1564-6a93-48c5-b51b-c436e1241f77',
+    '/bl': 'https://beatleader.com/u/76561198838652679',
+    '/ss': 'https://scoresaber.com/u/76561198838652679',
+    '/ssr': 'https://ssr.fascinated.cc/player/76561198838652679',
+
     // other
     '/refs': 'https://drive.google.com/drive/folders/1m2yGpN1UeGEGOsTOthRqPdLm1SSQaL1I',
   },
